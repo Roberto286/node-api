fe8671e6b99b374d38202648af9418288a842757
Roberto286 <roberto.saliola96@gmail.com>
2022-07-02 15:33:28 +0200

    creating crud operation methods

---
+++ src/controllers/Quote.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
+import Quote from "../models/Quote";
+
+const createQuote = (req:Request, res: Response, next: NextFunction) => {
+    const {body} = req.body;
+
+    const quote = new Quote({
+        _id: new mongoose.Types.ObjectId(),
+        body
+    });
+
+    return quote
+        .save()
+        .then((quote) => res.status(201).json({quote}))
+        .catch((err) => res.status(500).json({err}));
+}
+
+const readQuote = (req:Request, res: Response, next: NextFunction) => {
+
+}
+
+const readRandomQuote = (req:Request, res: Response, next: NextFunction) => {
+
+}
+
+const readAllQuotes = (req:Request, res: Response, next: NextFunction) => {
+
+}
+
+const readQuoteByAuthor = (req:Request, res: Response, next: NextFunction) => {
+
+}
+
+const updateQuote = (req:Request, res: Response, next: NextFunction) => {};
+
+const deleteQuote = (req:Request, res: Response, next: NextFunction) => {
+
+}
