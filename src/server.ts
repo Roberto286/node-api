3f6ca401e88f67496190a49b40c7fa394f7bb9db
Roberto286 <roberto.saliola96@gmail.com>
2022-07-02 15:32:19 +0200

    improved db connection

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
 import {config} from './config/config';
+import Logging from './library/Logging';
 
 const router = express();
 
 //connect to mongodb
 mongoose.connect(config.mongo.url, {retryWrites: true, w: 'majority'})
-.then(() => {console.log('connected!')})
-.catch(e => {console.log(e.message)})
+.then(() => {Logging.info('Connected to mongodb')})
+.catch(e => {Logging.error('Unable to connect: ');
+             Logging.error(e.message)})
