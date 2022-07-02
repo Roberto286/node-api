99419669a03d1cd7f5c15ae4e6eb4f61b00de2f0
Roberto286 <roberto.saliola96@gmail.com>
2022-07-02 15:32:40 +0200

    full server connection with logging

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,58 @@ const router = express();
 
 //connect to mongodb
 mongoose.connect(config.mongo.url, {retryWrites: true, w: 'majority'})
-.then(() => {Logging.info('Connected to mongodb')})
-.catch(e => {Logging.error('Unable to connect: ');
-             Logging.error(e.message)})
+.then(() => {
+    Logging.info('Connected to mongodb');
+    StartServer();
+})
+.catch(e => {
+    Logging.error('Unable to connect: ');
+    Logging.error(e.message)
+});
+
+//only start server if db is connected
+const StartServer = () => {
+    router.use((req,res,next) => {
+        Logging.info(`incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            Logging.info(`incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${req.statusCode}]`);
+        })
+
+        next();
+    });
+
+    router.use(express.urlencoded({extended: true}));
+    router.use(express.json());
+
+    /** Rules of API */
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Routes */
+
+    /** Healthcheck */
+    router.get('/ping', (req, res, next) => res.status(200).json({ hello: 'world' }));
+
+    /** Error handling */
+    router.use((req, res, next) => {
+        const error = new Error('Not found');
+
+        Logging.error(error);
+
+        res.status(404).json({
+            message: error.message
+        });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
+};
