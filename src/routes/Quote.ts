f87afd02a026b52e05b093b3d604c3136a3247d6
Roberto286 <roberto.saliola96@gmail.com>
2022-07-03 16:09:01 +0200

    defined all routes (find by text missing)

--- src/routes/Quote.ts
+++ src/routes/Quote.ts
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.post('/create', controller.createQuote);
 router.get('/', controller.readAllQuotes);
-
+router.get('/random', controller.readRandomQuote);
+router.get('/by-author/:authorName', controller.readQuoteByAuthor);
+router.patch('/update/:quoteId', controller.updateQuote);
+router.delete('/delete/:quoteId', controller.deleteQuote);
 export = router;
