4750c990b83ad980fc6b173ddf3a356dac08f278
Roberto286 <roberto.saliola96@gmail.com>
2022-07-03 16:08:32 +0200

    defined all crud operations (find by text missing)

--- src/controllers/Quote.ts
+++ src/controllers/Quote.ts
@@ -7,7 +7,10 @@ const createQuote = (req:Request, res: Response, next: NextFunction) => {
 
     const quote = new Quote({
         _id: new mongoose.Types.ObjectId(),
-        body
+        authorId : `${req.body.authorId}`,
+        authorName : `${req.body.authorName}`,
+        quoteText : `${req.body.quoteText}`,
+        date : `${req.body.date ? req.body.date : new Date()}`
     });
 
     return quote
@@ -16,11 +19,14 @@ const createQuote = (req:Request, res: Response, next: NextFunction) => {
         .catch((err) => res.status(500).json({err}));
 }
 
-const readQuote = (req: Request, res: Response, next: NextFunction) => {};
+const readQuote = (req: Request, res: Response, next: NextFunction) => {
+
+};
 
 const readRandomQuote = (req:Request, res: Response, next: NextFunction) => {
-
-}
+    return Quote.aggregate([{$sample:{size:1}}])
+        .then((quote) => res.status(200).json({ quote }))
+        .catch((err) => res.status(500).json({ err }));}
 
 const readAllQuotes = (req:Request, res: Response, next: NextFunction) => {
     return Quote.find()
@@ -30,13 +36,39 @@ const readAllQuotes = (req:Request, res: Response, next: NextFunction) => {
 }
 
 const readQuoteByAuthor = (req:Request, res: Response, next: NextFunction) => {
-
+    const authorName = req.params.authorName;
+    return Quote.aggregate([
+        { $match: { authorName : new RegExp('^'+authorName+'$', "i") } },
+        { $sample:{size:1}}
+    ])
+        .then((quote) => res.status(200).json({ quote }))
+        .catch((err) => res.status(500).json({ err }))
 }
 
-const updateQuote = (req:Request, res: Response, next: NextFunction) => {};
+const updateQuote = (req:Request, res: Response, next: NextFunction) => {
+    const quoteId = req.params.quoteId;
+
+    return Quote.findById(quoteId)
+        .then((quote) => {
+            if (quote) {
+                quote.set(req.body);
+
+                return quote
+                    .save()
+                    .then((quote) => res.status(201).json({ quote }))
+                    .catch((error) => res.status(500).json({ error }));
+            } else {
+                return res.status(404).json({ message: 'not found' });
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
 
 const deleteQuote = (req:Request, res: Response, next: NextFunction) => {
+    const quoteId = req.params.quoteId;
 
-}
+    return Quote.findByIdAndDelete(quoteId)
+        .then((quote) => (quote ? res.status(201).json({ author: quote, message: 'Deleted' }) : res.status(404).json({ message: 'not found' })))
+        .catch((error) => res.status(500).json({ error }));}
 
-export default { createQuote, readAllQuotes };
+export default { createQuote, readAllQuotes, readRandomQuote, readQuoteByAuthor, updateQuote, deleteQuote };
